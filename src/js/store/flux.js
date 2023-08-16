2593b4afdb1f3797811dc6e5a8567afa37b49a8f
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-08-16 23:33:04 +0000

    webpack-dev-server error

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,29 +1,46 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: [],
+			planets: [],
+			vehicles: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
+			fetchGetAllPeople: () => {
+				fetch("https://www.swapi.tech/api/people")
+				.then(response => response.json())	
+				.then(data => {
+					console.log(data);
+				})
+				.catch(error => console.log("Something went wrong, check me out:", error))
+			},
+			// fetchGetAllPlanets: () => {
+			// 	fetch("https://www.swapi.tech/api/planets")
+			// 	.then(response => response.json())	
+			// 	.then(data => {console.log(data)})
+			// 	.catch(error => console.log("Something went wrong, check me out:", error))
+			// },
+			// fetchGetAllVehicles: () => {
+			// 	fetch("https://www.swapi.tech/api/vehicles")
+			// 	.then(response => response.json())	
+			// 	.then(data => {
+			// 		console.log(data);
+			// 	})
+			// 	.catch(error => console.log("Something went wrong, check me out:", error))
+			// },
+			initialLoading: () => {
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
+				getActions().fetchGetAllPeople();
+				//getActions().fetchGetAllPlanets();
+				//getActions().fetchGetAllVehicles();
 			},
+			
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
